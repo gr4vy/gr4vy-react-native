1060e5c68b3fc82e339c58141f5c7f8b8bbcc71a
Luca Allievi <luca@gr4vy.com>
2024-12-18 18:31:52 +0100

    Upgrade native stuff and rest of the dependencies / configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'prettier'],
+  extends: ['@react-native', 'prettier'],
   rules: {
     'prettier/prettier': [
       'error',
